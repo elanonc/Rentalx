17bc0c7592a7fa2780cfb72e1b3daf05217e06c8
Elano N. Caitano <nunes.elano14@gmail.com>
2022-07-01 20:30:57 -0300

    Definindo as categorias

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes); // assim os métodos criados no categoriesRoutes terão como path o /categories
 
 app.listen(3333);
