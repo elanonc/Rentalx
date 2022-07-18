37d08c178a37c72168c8278a421ad58c3c741549
Elano N. Caitano <nunes.elano14@gmail.com>
2022-07-17 21:08:52 -0300

    Refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import express from "express";
+import { router } from "./routes/"
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes); // assim os métodos criados no categoriesRoutes terão como path o /categories
-app.use("/specifications", specificationsRoutes); 
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running!"));
