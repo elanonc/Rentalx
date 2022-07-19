6dd1ff38a8751b2f8aa80ec0be61429947e195e0
Elano N. Caitano <nunes.elano14@gmail.com>
2022-07-18 22:49:54 -0300

    Criando upload de arquivos utilizando multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,16 @@
-import { Router } from 'express';
+import { Router } from "express";
+import multer from "multer";
 
-import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategory';
 
-const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
+const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp", // destino
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -15,5 +19,9 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
 });
+                                 // single para receber apenas um arquivo por vez
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoryController.handle(request, response);
+})
 
 export { categoriesRoutes };
