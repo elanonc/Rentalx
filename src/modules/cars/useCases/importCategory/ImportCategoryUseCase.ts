f54b7c2b9ab70046f54c6134717ddecf8123dea0
Elano N. Caitano <nunes.elano14@gmail.com>
2022-07-21 21:33:26 -0300

    Introdução ao conceito de Stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,20 @@
+import {parse} from "csv-parse";
+
+import fs from "fs";
+
+
 class ImportCategoryUseCase {
-    execute(file:any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        const stream = fs.createReadStream(file.path); // utilizando essa função, podemos ler o arquivo por partes
+
+        //const parseFile = csvParse();
+        const parseFile = parse();
+
+        stream.pipe(parseFile); // a cada "parte" do arquivo lido, o pipe possa enviar essa parte pra onde vamos determinar 
+
+        parseFile.on("data", async(line) => {
+            console.log(line);
+        });
     }
 } 
 
